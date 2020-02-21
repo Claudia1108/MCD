eb7247102012538f125ee4a7c1e77e3dd7b619eb
Claudia1108 <58569195+Claudia1108@users.noreply.github.com>
2020-02-21 15:02:51 -0600

    Add files via upload

---
+++ 01_arreglo2x2.py
@@ -0,0 +1,6 @@
+#1.	Crear arreglo de 2x2
+
+import numpy as np
+b = np.ones((2,2))    
+print(b)                     
+
---
+++ 02_arregloDimension.py
@@ -0,0 +1,5 @@
+#2.	Devolver dimension/forma del arreglo
+import numpy as np
+b = np.array([[1,2],[4,5]])    
+print(b.shape)
+
